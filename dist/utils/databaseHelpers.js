1699ed8981300bd61caa657da17d3f4c5980cd7e
Diego Antonelli <diego.antonelli@hotmail.com>
2020-06-10 11:04:22 +0200

    Removendo dist folder

--- dist/utils/databaseHelpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateSort = void 0;
-exports.generateSort = (key, descending = false) => {
-    return {
-        [key]: descending ? -1 : 1,
-    };
-};
